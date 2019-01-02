cac70454c723be8d64c912c2211c9f94a499436e
YOON HYUKJIN <heokjin@gmail.com>
2019-01-02 23:58:50 +0900

    dev

--- vue/js/ssamhandmade.js
+++ vue/js/ssamhandmade.js
@@ -19,7 +19,7 @@ var app3 = new Vue({
   }
 })
 
-var app4 = new Vue({
+new Vue({
   el: '#app-4',
   data: {
     todos: [
@@ -66,5 +66,169 @@ var app7 = new Vue({
       { id: 1, text: 'Cheese' },
       { id: 2, text: 'Whatever else humans are supposed to eat' }
     ]
+  }
+})
+
+var obj = {
+  foo: 'bar'
+}
+Object.freeze(obj)
+new Vue({
+  el: '#app-8',
+  data: obj
+})
+
+new Vue({
+  data: {
+    a: 1,
+  },
+  created: function () {
+    // `this` 는 vm 인스턴스를 가리킵니다.
+    console.log('a is: ' + this.a)
+  }
+})
+
+var vm = new Vue({
+  el: '#example',
+  data: {
+    message: '안녕하세요'
+  },
+  computed: {
+    // 계산된 getter
+    reversedMessage: function () {
+      // `this` 는 vm 인스턴스를 가리킵니다.
+      return this.message.split('').reverse().join('')
+    }
+  }
+})
+
+//var vm = new Vue({
+//  el: '#demo',
+//  data: {
+//    firstName: 'Foo',
+//    lastName: 'Bar',
+//    fullName: 'Foo Bar'
+//  },
+//  watch: {
+//    firstName: function (val) {
+//      this.fullName = val + ' ' + this.lastName
+//    },
+//    lastName: function (val) {
+//      this.fullName = this.firstName + ' ' + val
+//    }
+//  }
+//})
+//위에스크립트와 동일, 선언적이냐 명령적이냐 차이
+var vm = new Vue({
+  el: '#demo',
+  data: {
+    firstName: 'Foo',
+    lastName: 'Bar'
+  },
+  computed: {
+    fullName: function () {
+      return this.firstName + ' ' + this.lastName
+    }
+  }
+})
+
+var watchExampleVM = new Vue({
+  el: '#watch-example',
+  data: {
+    question: '',
+    answer: '질문을 하기 전까지는 대답할 수 없습니다.'
+  },
+  watch: {
+    // 질문이 변경될 때 마다 이 기능이 실행됩니다.
+    question: function (newQuestion) {
+      this.answer = '입력을 기다리는 중...'
+      this.getAnswer()
+    }
+  },
+  methods: {
+    // _.debounce는 lodash가 제공하는 기능으로
+    // 특히 시간이 많이 소요되는 작업을 실행할 수 있는 빈도를 제한합니다.
+    // 이 경우, 우리는 yesno.wtf/api 에 액세스 하는 빈도를 제한하고,
+    // 사용자가 ajax요청을 하기 전에 타이핑을 완전히 마칠 때까지 기다리길 바랍니다.
+    // _.debounce 함수(또는 이와 유사한 _.throttle)에 대한
+    // 자세한 내용을 보려면 https://lodash.com/docs#debounce 를 방문하세요.
+    getAnswer: _.debounce(
+      function () {
+        if (this.question.indexOf('?') === -1) {
+          this.answer = '질문에는 일반적으로 물음표가 포함 됩니다. ;-)'
+          return
+        }
+        this.answer = '생각중...'
+        var vm = this
+        axios.get('https://yesno.wtf/api')
+          .then(function (response) {
+            vm.answer = _.capitalize(response.data.answer)
+          })
+          .catch(function (error) {
+            vm.answer = '에러! API 요청에 오류가 있습니다. ' + error
+          })
+      },
+      // 사용자가 입력을 기다리는 시간(밀리세컨드) 입니다.
+      500
+    )
+  }
+})
+
+var example2 = new Vue({
+  el: '#example-2',
+  data: {
+    parentMessage: 'Parent',
+    items: [
+      { message: 'Foo' },
+      { message: 'Bar' }
+    ]
+  }
+})
+
+Vue.component('todo-item2', {
+  template: '\
+    <li>\
+      {{ title }}\
+      <button v-on:click="$emit(\'remove\')">X</button>\
+    </li>\
+  ',
+  props: ['title']
+})
+
+new Vue({
+  el: '#todo-list-example',
+  data: {
+    newTodoText: '',
+    todos: [
+      {
+        id: 1,
+        title: 'Do the dishes',
+      },
+      {
+        id: 2,
+        title: 'Take out the trash',
+      },
+      {
+        id: 3,
+        title: 'Mow the lawn'
+      }
+    ],
+    nextTodoId: 4
+  },
+  methods: {
+    addNewTodo: function () {
+      this.todos.push({
+        id: this.nextTodoId++,
+        title: this.newTodoText
+      })
+      this.newTodoText = ''
+    }
+  }
+})
+
+var example1 = new Vue({
+  el: '#example-1',
+  data: {
+    counter: 0
   }
 })
